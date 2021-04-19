8b399a7a48679e42e1734514a57de2dae7a3a321
GyuJung Kim <pandora@crazyguys.me>
2021-04-19 23:33:46 +0900

    @property 사용한 버전으로 변경

--- python3/rebalancing.py
+++ python3/rebalancing.py
@@ -7,11 +7,11 @@ from data import Data
 
 def run(data: Data):
     # 현금이 주식 평가금액보다 30% 이상 많아지면
-    if data.get_money() > data.valuation_amount() * 1.3:
+    if data.money > data.valuation_amount * 1.3:
         # 현금이 많음으로 가진 현금의 15%를 주식 매수
         data.buy(15)
     # 반대로 현금이 주식 평가금액보다 30% 이상 적어지면
-    elif data.get_money() * 1.3 < data.valuation_amount():
+    elif data.money * 1.3 < data.valuation_amount:
         # 현금이 적음으로 가진 주식의 15%를 매도
         data.sell(15)
 
